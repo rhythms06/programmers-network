81aaafd74d7f9a8888da4811cc026c438c2ace67
Sakib Rasul <rhythms06@gmail.com>
2016-04-11 19:02:54 -0400

    Wrote getAllParams(), which funnels query data from the dashbord into a local char array.

--- seefriends.c
+++ seefriends.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Helper macro to convert two-character hex strings to character value
+#define ToHex(Y) (Y>='0'&&Y<='9'?Y-'0':Y-'A'+10)
+
 // will capture information recieved from dashboard.py
 char InputData[4096];
 
@@ -25,7 +28,20 @@ int main(void)
 
 /* parse all recieved information */
 void getAllParams() {
-
+  // Determines if it is a POST or GET method
+	if( getenv( "REQUEST_METHOD" ) == 0 ) {
+		printf("No REQUEST_METHOD, must be running in DOS mode\n");
+		return;
+	} else if (strcmp( getenv("REQUEST_METHOD"), "POST") == 0) {
+		// If POST
+		char *endptr;	// quite useless, but required
+		char *len1 = getenv("CONTENT_LENGTH");
+		int contentlength = strtol(len1, &endptr, 10);
+		fread(InputData , contentlength, 1, stdin);
+	} else {
+		// If GET
+		strcpy(InputData, getenv("QUERY_STRING"));
+	}
 }
 
 /* parse specific key into a variable name for later use */
