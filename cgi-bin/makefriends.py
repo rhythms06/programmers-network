cc31220209d824ba92df478d8f8279b6ffb38059
Sakib Rasul <rhythms06@gmail.com>
2016-04-11 21:35:23 -0400

    Added code to recieve the current username.

--- cgi-bin/makefriends.py
+++ cgi-bin/makefriends.py
@@ -6,3 +6,15 @@
 # Date: April 2016
 # File: makefriends.py
 #
+
+# cgi to receive input from previous page
+import cgi
+# sys to take input from terminal (mainly for testing)
+import sys
+
+# Get the username that was submitted via CGI or via command-line
+if len(sys.argv) > 1:
+	username = sys.argv[1]
+else:
+	form = cgi.FieldStorage();
+	username = form.getvalue("username")
